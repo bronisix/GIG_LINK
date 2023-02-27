1cfdab5d1e8346ec8b58b0b8b38844ab3fd5ad75
Hugo Peyron <hugopeyron@gmail.com>
2023-02-27 17:39:45 +0100

    added the navbar + remove the end in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
     resources :events, only: [:index, :show, :new, :create, :edit, :update]
     end
     resources :events, only: [:destroy]
-  end
+end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
