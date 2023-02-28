4fbd7146c453c8ca221ea44262a9cd3dc5be25ec
Guillaume de Malzac <guillaume.de.malzac@gmail.com>
2023-02-28 13:05:02 +0100

    j'ai ajouté 4 seeds de bars avec 4 seeds d'users qui ont chacun leur bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,22 @@
 
 puts "Cleaning database..."
 Bar.destroy_all
+User.destroy_all
 
 puts "Creating user..."
-john = User.create!(username: "John", email: "john@hello.freu", password: "AZERTY")
+guillaume = User.create!(username: "Guillaume", email: "guillaume@hello.fr", password: "123456")
+audrey = User.create!(username: "Audrey", email: "audrey@hello.fr", password: "123456")
+hugo = User.create!(username: "Hugo", email: "hugo@hello.fr", password: "123456")
+robin = User.create!(username: "Robin", email: "robin@hello.fr", password: "123456")
 puts "user created"
 
 puts "Creating bars..."
-les_marquises = { name: "Mes Marquises", url: "wwww.lesmarquises.freu", telephone: "01 42 34 56 78", user: john }
-cafe_pop = { name: "Pizza East", url: "wwww.lecafepopulaire.usofa", telephone: "01 44 24 78 78", user: john}
+les_marquises = { name: "Les Marquises (Guillaume)", url: "wwww.lesmarquises.freu", telephone: "01 42 34 56 78", user: guillaume }
+cafe_pop = { name: "Pizza East (Audrey)", url: "wwww.lecafepopulaire.usofa", telephone: "01 44 24 78 78", user: audrey }
+doc_feelgood = { name: "Docteur Feelgood (Hugo)", url: "wwww.drfg.de", telephone: "08 23 40 90 12", user: hugo }
+dilf = { name: "Les DILF (Robin)", url: "wwww.lesdilfs.xxx", telephone: "06 09 12 47 94", user: robin }
 
-[les_marquises, cafe_pop].each do |attributes|
+[les_marquises, cafe_pop, doc_feelgood, dilf].each do |attributes|
   bar = Bar.create!(attributes)
   puts "Created #{bar.name}"
 end
