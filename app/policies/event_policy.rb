6295ff1e8caf7bc6a7f2c5064bf819df72ac7adf
Robin SIX <robin.six@gmail.com>
2023-02-28 15:51:38 +0100

    pundit pour les events

---
+++ app/policies/event_policy.rb
@@ -0,0 +1,24 @@
+class EventPolicy < ApplicationPolicy
+  
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
