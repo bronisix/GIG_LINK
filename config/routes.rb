f91c1f396b890e74df2a7f0b62fa3978c05066a2
Guillaume de Malzac <guillaume.de.malzac@gmail.com>
2023-03-02 13:08:17 +0100

    on a fait la page events

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :bars do
-    resources :events, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :events, only: [:new, :create, :edit, :update] do
       member do
         patch :accept
         patch :decline
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     end
   end
   resources :events, only: [:destroy]
+  get "my-own-events", to: "events#my_own_events"
   get 'mybars', to: 'bars#mybars'
 end
 
